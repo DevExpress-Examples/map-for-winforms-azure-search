b1ff5157e736807ec6dec4da31d16b0d14e6b619
ritazakhodyaeva <57389482+ritazakhodyaeva@users.noreply.github.com>
2024-11-14 20:34:05 +0400

    Update Form1.cs

--- CS/AzureMapSearch/Form1.cs
+++ CS/AzureMapSearch/Form1.cs
@@ -3,51 +3,39 @@ using DevExpress.XtraPrinting.Native;
 using System.Text;
 
 namespace AzureMapSearch {
-    public partial class Form1 : Form
-    {
+    public partial class Form1 : Form {
         const string azureKey = "your key";
         AzureSearchDataProvider azureSearchProvider;
-        public Form1()
-        {
+        public Form1() {
             InitializeComponent();
-            imageLayer1.DataProvider = new AzureMapDataProvider()
-            {
+            imageLayer1.DataProvider = new AzureMapDataProvider() {
                 AzureKey = azureKey,
                 Tileset = AzureTileset.Imagery
             };
-
-            imageLayer2.DataProvider = new AzureMapDataProvider()
-            {
+            imageLayer2.DataProvider = new AzureMapDataProvider(){
                 AzureKey = azureKey,
                 Tileset = AzureTileset.BaseLabelsRoad,
             };
-            azureSearchProvider = new AzureSearchDataProvider()
-            {
+            azureSearchProvider = new AzureSearchDataProvider(){
                 AzureKey = azureKey,
             };
             informationLayer1.DataProvider = azureSearchProvider;
             informationLayer1.DataRequestCompleted += OnDataRequestCompleted;
             azureSearchProvider.SearchCompleted += new AzureSearchCompletedEventHandler(OnSearchCompleted);
             mapControl1.SearchPanelOptions.Visible = false;
-
         }
-        void OnDataRequestCompleted(object sender, RequestCompletedEventArgs e)
-        {
+        void OnDataRequestCompleted(object sender, RequestCompletedEventArgs e) {
             mapControl1.ZoomToFitLayerItems(0.4);
         }
-        void OnSearchCompleted(object sender, AzureSearchCompletedEventArgs e)
-        {
+        void OnSearchCompleted(object sender, AzureSearchCompletedEventArgs e) {
             if (e.Cancelled) return;
-            if (e.RequestResult.ResultCode != RequestResultCode.Success)
-            {
+            if (e.RequestResult.ResultCode != RequestResultCode.Success) {
                 memoEdit1.Text = "The Bing Search service does not work for this location.";
                 return;
             }
-
             StringBuilder resultList = new StringBuilder("");
             int resCounter = 1;
-            foreach (LocationInformation resultInfo in e.RequestResult.SearchResults)
-            {
+            foreach (LocationInformation resultInfo in e.RequestResult.SearchResults) {
                 resultList.Append(String.Format("Result {0}:  \r\n", resCounter));
                 resultList.Append(String.Format("Address: {0}\r\n", resultInfo.Address.FormattedAddress));
                 resultList.Append(String.Format("Geographic coordinates:  {0}\r\n", resultInfo.Location));
@@ -56,7 +44,6 @@ namespace AzureMapSearch {
             }
             memoEdit1.Text = resultList.ToString();
         }
-
         private void simpleButton1_Click(object sender, EventArgs e) {
             azureSearchProvider.Search(textEdit1.Text, maxResults: 2);
         }
